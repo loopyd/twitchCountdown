de9a6edc9b8bd66ce7b1b45fbc5f655bbff86d8e
saber7ooth <nightwintertooth@gmail.com>
2022-09-24 04:53:10 -0700

    Massive update

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # Twitch Countdown Timer
-A very simple countdown timer to be used as a browser source
+A very good looking countdown timer.
 
-## Usage
+## Customizing the Timer
+You can follow this guide to customize your timer to your liking.
 
-May add custom commands/interface in the future but for now the style sheet contains all the colour editing in RGBA format (standard CSS) and changing the values of the following:
+### Colors
+You may edit any ``color:`` line in clock.css to customize the countdown timer's color scheme.
 
-    var initialDate
-    var countDownDate
+### Fonts
+If you want to use another google font, modify the line below in ``index.html``:
+
+```html
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+```
+
+You can find fonts to use on [Google Fonts](https://fonts.google.com/).
+
+### Countdown time
+
+The countdown timer is specified by adding GET arguments to the browser source URL.
+
+GET variables you can add to index.html to customize the timer:
+
+| --- | --- |
+| Variable | Description            | 
+| --- | --- |
+| d | Days |
+| h | Hours |
+| m | Minutes |
+| s | Seconds |
+| --- | --- |
 
 ## "Installation"
 
-You don't really install this, just download the ZIP (got to the green code button at top of page), place where convenient and add 'index.html'as a browser source in OBS/SLOBS/etc..
+You don't really install this, just download the ZIP (got to the green code button at top of page), place where convenient and add 'index.html' as a browser source in OBS/SLOBS/etc, and pass in your countdown time as URL GET parameters.
--- js/countdown.js
+++ js/countdown.js
@@ -1,35 +1,190 @@
-var initialDate = new Date("October 4, 2020").getTime();
-var countDownDate = new Date("November 4, 2020").getTime();
+var GetVars;
+var cooldown_config;
+var initialDate;
+var countdownDate;
+var totalDistance;
+var colors;
+var frame;
 
-// Update the count down every 1 second
-var x = setInterval(function () {
+function mapRange(from, to, s) {
+    return to[0] + (s - from[0]) * (to[1] - to[0]) / (from[1] - from[0]);
+}
 
-    var now = new Date().getTime();
+function oscillator(time, frequency = 1, amplitude = 1, phase = 0, offset = 0){
+    return Math.sin(time * frequency * Math.PI * 2 + phase * Math.PI * 2) * amplitude + offset; 
+}
 
-    // Find the distances
-    var distance = countDownDate - now;
-    var totalDistance = countDownDate - initialDate;
-
-    // time calcs
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // display remaining time
-    document.getElementById("puppyText").innerHTML = days + " days until puppy!";
-
-    // if count down is finished, write some text and set border
-    if (distance <= 0) {
-        clearInterval(x);
-        document.getElementById("puppyText").innerHTML = "PUPPY TIME!";
+function GetDivCSSColor(fromClass) {
+    var $inspector = $("<div>").css('display', 'none').addClass(fromClass);
+    $("body").append($inspector); // add to DOM, in order to read the CSS property
+    try {
+        var color = $inspector.css('color');
+        return color;
+    } finally {
+        $inspector.remove(); // and remove from DOM
     }
-    updateBar(distance, totalDistance);
-}, 1000); // <- this is the update interval 
+};
 
-// function to update progress bar
+// Function to parse GET variables.
+function parseGet() {
+    var $_GET = {};
+    if (document.location.toString().indexOf('?') !== -1) {
+        var query = document.location
+            .toString()
+            .replace(/^.*?\?/, '')
+            .replace(/#.*$/, '')
+            .split('&');
+        for (var i = 0, l = query.length; i < l; i++) {
+            var aux = decodeURIComponent(query[i]).split('=');
+            $_GET[aux[0]] = aux[1];
+        }
+    }
+    return $_GET;
+}
+
+// Function to get the amount of time remaining.
+function getTimeRemaining(endtime) {
+    const total = endtime - new Date().getTime();
+    const milliseconds = Math.floor( total/10 );
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+    return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds,
+        milliseconds
+    };
+}
+
+// Function to update the clock portion of the UI.
+function updateClock(endtime) {
+    const t = getTimeRemaining(endtime);
+    var clock = document.getElementById("timer");
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+    const millisecondsSpan = clock.querySelector('.milliseconds');
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    millisecondsSpan.innerHTML = ('0' + t.milliseconds).slice(-2);
+}
+
+// Update the colors of the clock based on three different sine wave oscillators to
+// add some visual flair!
+function updateColors(_currentDistance, _totalDistance) {
+    let percentage = (_totalDistance - _currentDistance) / _totalDistance;
+    if (percentage <= 0.75) {
+        index = Math.round(
+            oscillator((totalDistance * percentage) / 1000, 0.125, 16, 0, 16)
+        );
+        $('#timer > span.hours').css('color', colors.hours.long[index]);
+        $('#timer > span.minutes').css('color', colors.minutes.long[index]);
+        $('#timer > span.seconds').css('color', colors.seconds.long[index]);
+        $('#timer > span.milliseconds').css('color', colors.milliseconds.long[index]);
+        $('#timer > span.seperator').css('color', colors.seperator.long[index]);
+    } else if (percentage > 0.75 && percentage <= .85) {
+        index = Math.round(
+            oscillator((totalDistance * percentage) / 1000, 0.5, 16, 0, 16)
+        );
+        $('#timer > span.hours').css('color', colors.hours.mid[index]);
+        $('#timer > span.minutes').css('color', colors.minutes.mid[index]);
+        $('#timer > span.seconds').css('color', colors.seconds.mid[index]);
+        $('#timer > span.milliseconds').css('color', colors.milliseconds.mid[index]);
+        $('#timer > span.seperator').css('color', colors.seperator.mid[index]);
+    } else {
+        index = Math.round(
+            oscillator((totalDistance * percentage) / 1000, 1, 16, 0, 16)
+        );
+        $('#timer > span.hours').css('color', colors.hours.short[index]);
+        $('#timer > span.minutes').css('color', colors.minutes.short[index]);
+        $('#timer > span.seconds').css('color', colors.seconds.short[index]);
+        $('#timer > span.milliseconds').css('color', colors.milliseconds.short[index]);
+        $('#timer > span.seperator').css('color', colors.seperator.short[index]);
+    }
+}
+
+// Function to update the progress bar portion of the UI.
 function updateBar(_currentDistance, _totalDistance) {
-    let percentage = Math.round(((_totalDistance - _currentDistance) / _totalDistance) * 100);
+    var bar = document.getElementById("bar");
+    let percentage = ((_totalDistance - _currentDistance) / _totalDistance) * 100;
     percentage = (percentage < 100) ? percentage : 100;
     bar.style.width = `${percentage}%`;
 } 
+
+// Function to update the timer UI.
+function upodateUI(timerHandle) {
+    var distance = countDownDate - new Date().getTime();
+    updateClock(countDownDate);
+    if (distance <= 0) {
+        clearInterval(timerHandle);
+        var clock = document.getElementById("timer");
+        clock.innerHTML = "STARTING NOW!";
+    }
+    updateBar(distance, totalDistance);
+    updateColors(distance, totalDistance);
+}
+
+// Countdown main thread.
+$(document).ready(function () {
+    var GetVars = parseGet();
+    var countdown_config = {
+        days: parseInt((!GetVars['d']) ? 0 : GetVars['d']),
+        days_enabled: !(!GetVars['d']),
+        hours: parseInt((!GetVars["h"]) ? 0 : GetVars['h']),
+        hours_enabled: !(!GetVars['h']),
+        minutes: parseInt((!GetVars['m']) ? 0 : GetVars['m']),
+        minutes_enabled: !(!GetVars['m']),
+        seconds: parseInt((!GetVars['s']) ? 0 : GetVars['s']),
+        seconds_enabled: !(!GetVars['s'])
+    }
+    if (!countdown_config.seconds_enabled && !countdown_config.minutes_enabled && !countdown_config.hours_enabled && !countdown_config.days_enabled) {
+        throw new Error("No time specified in GET variables.  You need to set a countdown time.");
+    }
+    initialDate = new Date().getTime();
+    countDownDate = new Date(initialDate +
+        (1000 * countdown_config.seconds) +
+        (1000 * 60 * countdown_config.minutes) +
+        (1000 * 60 * 60 * countdown_config.hours)
+    ).getTime();
+    totalDistance = countDownDate - initialDate;
+    colors = {
+        hours: {
+            short: chroma.scale([$('#timer > span.hours').css('color'), '#11ff11']).mode('lch').colors(32),
+            mid: chroma.scale([$('#timer > span.hours').css('color'), '#ffff11']).mode('lch').colors(32),
+            long: chroma.scale([$('#timer > span.hours').css('color'), '#ff1111']).mode('lch').colors(32)
+        },
+        minutes: {
+            short: chroma.scale([$('#timer > span.minutes').css('color'), '#11ff11']).mode('lch').colors(32),
+            mid: chroma.scale([$('#timer > span.minutes').css('color'), '#ffff11']).mode('lch').colors(32),
+            long: chroma.scale([$('#timer > span.minutes').css('color'), '#ff1111']).mode('lch').colors(32)
+        },
+        seconds: {
+            short: chroma.scale([$('#timer > span.seconds').css('color'), '#11ff11']).mode('lch').colors(32),
+            mid: chroma.scale([$('#timer > span.seconds').css('color'), '#ffff11']).mode('lch').colors(32),
+            long: chroma.scale([$('#timer > span.seconds').css('color'), '#ff1111']).mode('lch').colors(32)
+        },
+        milliseconds: {
+            short: chroma.scale([$('#timer > span.milliseconds').css('color'), '#11ff11']).mode('lch').colors(32),
+            mid: chroma.scale([$('#timer > span.milliseconds').css('color'), '#ffff11']).mode('lch').colors(32),
+            long: chroma.scale([$('#timer > span.milliseconds').css('color'), '#ff1111']).mode('lch').colors(32)
+        },
+        bar: {
+            short: chroma.scale([$('#bar').css('color'), '#11ff11']).mode('lch').colors(32),
+            mid: chroma.scale([$('#bar').css('color'), '#ffff11']).mode('lch').colors(32),
+            long: chroma.scale([$('#bar').css('color'), '#ff1111']).mode('lch').colors(32)
+        },
+        seperator: {
+            short: chroma.scale([$('#timer > span.seperator').css('color'), '#11ff11']).mode('lch').colors(32),
+            mid: chroma.scale([$('#timer > span.seperator').css('color'), '#ffff11']).mode('lch').colors(32),
+            long: chroma.scale([$('#timer > span.seperator').css('color'), '#ff1111']).mode('lch').colors(32)
+        }
+    }
+    var x = setInterval(function () {
+        upodateUI(x);
+    }, 1 );
+});
